3b0eacffa47a7f0b2a4442dd4b85a8f1d5361b58
00 <11>
2020-12-26 16:59:55 +0800

    2次提交

---
+++ changgou-common/src/main/java/com/changgou/util/Test.java
@@ -0,0 +1,8 @@
+package com.changgou.util;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("liuxiang");
+        System.out.println("1111");
+    }
+}
